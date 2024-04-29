6e73ce268668c8ee6da1f01b9884f9e70a407cec
Pravallikasunkireddy <162728262+Pravallikasunkireddy@users.noreply.github.com>
2024-04-29 20:21:59 +0530

    Commit 6

--- app/src/main/java/uk/ac/tees/mad/d3840711/ManageProfile.kt
+++ app/src/main/java/uk/ac/tees/mad/d3840711/ManageProfile.kt
@@ -1,12 +1,21 @@
 package uk.ac.tees.mad.d3840711
 
+import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
@@ -19,10 +28,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import coil.compose.rememberImagePainter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import uk.ac.tees.mad.d3840711.ui.theme.SheSafeTheme
+import java.io.ByteArrayOutputStream
 
 class ManageProfile : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -113,7 +126,10 @@ class ManageProfile : ComponentActivity() {
                         modifier = Modifier
                             .size(150.dp)
                             .clip(CircleShape)
-                            .border(2.dp, Color.Red, CircleShape),
+                            .border(2.dp, Color.Red, CircleShape)
+                            .clickable{
+                                triggerCamera()
+                            },
                         contentScale = ContentScale.Crop
                     )
 
@@ -212,5 +228,129 @@ class ManageProfile : ComponentActivity() {
             }
         }
     }
+
+
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun triggerCamera() {
+        if (checkPermissionsCamera()) {
+            if (isCameraPermissionEnabled()) {
+                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                try {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                } catch (e: ActivityNotFoundException) {
+                    // display error state to the user
+                }}
+        }
+        else{
+            Log.e("man log" , "requesting for camera permission")
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, open the camera
+                triggerCamera()
+            } else {
+                // Permission denied, handle accordingly (show a message, etc.)
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun checkPermissionsCamera(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun isCameraPermissionEnabled(): Boolean {
+        val permission = Manifest.permission.CAMERA
+        val result = ContextCompat.checkSelfPermission(this, permission)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val CAMERA_PERMISSION_REQUEST_CODE = 123
+
+
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val storage = FirebaseStorage.getInstance()
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            //binding.image.setImageBitmap(imageBitmap)
+            val fileName = "image_${System.currentTimeMillis()}.jpg"
+            val storageRef = storage.reference.child("profile_photo").child(fileName)
+            // Convert the Bitmap to a byte array
+            val baos = ByteArrayOutputStream()
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
+
+            // Upload the image to Firebase Storage
+            val uploadTask = storageRef.putBytes(data)
+            uploadTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    storageRef.downloadUrl.addOnSuccessListener { uri ->
+                        val downloadUrl = uri.toString()
+
+                        val uid = FirebaseAuth.getInstance().getCurrentUser()?.getUid()
+                        if (uid != null) {
+                            FirebaseFirestore.getInstance().collection("superwomen").document(uid).get()
+                                .addOnSuccessListener { document ->
+                                    if (document.exists()) {
+                                        val sheData = document.toObject(SheModel::class.java)
+                                        if (sheData != null) {
+                                            // Update the user data
+                                            val user = SheModel(
+                                                sheData.name,
+                                                sheData.email,
+                                                sheData.emergencyPhone,
+                                                sheData.emergencyMessage,
+                                                downloadUrl,
+                                                sheData.lat,
+                                                sheData.long
+                                            )
+                                            FirebaseFirestore.getInstance().collection("superwomen").document(uid).set(user)
+                                        }
+                                    } else {
+                                        // The document does not exist
+                                    }
+                                }
+
+                        }
+                        Toast.makeText(this, "Image uploaded successfully", Toast.LENGTH_SHORT)
+                            .show()
+                    } } else {
+                    val exception = task.exception
+                }
+            }
+        }
+    }
+
 
 }
